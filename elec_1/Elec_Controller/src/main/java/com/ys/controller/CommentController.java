45c500fdcaacf56c1cbc70060d41b5cd2618abd8
YS-SONY <ys_pgm@163.com>
2017-12-23 20:04:19 +0800

    eclipse_spring_mybatis_2017_12_23

---
+++ elec_1/Elec_Controller/src/main/java/com/ys/controller/CommentController.java
@@ -0,0 +1,38 @@
+package com.ys.controller;
+
+import com.ys.service.CommentService;
+import com.ys.domain.Comment;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: SONY
+ * Date: 2017/11/2
+ * Time: 10:08
+ * To change this template use File | Settings | File Templates.
+ */
+@Controller
+public class CommentController {
+    @Autowired
+    private CommentService commentService;
+
+    @RequestMapping("hello")
+    public String getComment(String name, Model model){
+        System.out.println("CommentController.getComment");
+        Comment comment = commentService.getComment(2);
+        System.out.println(comment);
+        model.addAttribute("name",comment);
+
+        return "user/demo";
+
+    }
+
+    @RequestMapping("tx")
+    public String updateComment() {
+        commentService.updateComment(new Comment());
+        return "user/demo";
+    }
+}
---
+++ elec_1/Elec_Controller/src/main/webapp/index.jsp
@@ -0,0 +1,10 @@
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<body>
+<h2>Hello World!</h2>
+
+<a href="hello.html?name=你好">跳转</a>
+<br>
+<a href="tx.html">事务操作</a>
+</body>
+</html>
